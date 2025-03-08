03e76fe10581ba7da991d7e8e3d1793bc89f8aea
Tejas Goyal <117188164+TejasGoyal0@users.noreply.github.com>
2025-03-09 01:51:48 +0530

    Update models.py

--- app/models.py
+++ app/models.py
@@ -1,10 +1,2 @@
-from pydantic import BaseModel
-from typing import List
 
-class Order(BaseModel):
-    order_value: int
-    returned: bool
-
-class OrdersRequest(BaseModel):
-    transactions: List[Order]
 
